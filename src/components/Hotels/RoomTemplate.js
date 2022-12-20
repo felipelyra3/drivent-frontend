01d2682e5266481f4ef247c3f6270de8982e0eb1
Caio-Coldebella <c232621@dac.unicamp.br>
2022-12-20 20:58:14 -0300

    feat: quadro de selecao de quarto completo

--- src/components/Hotels/RoomTemplate.js
+++ src/components/Hotels/RoomTemplate.js
@@ -21,8 +21,7 @@ export default function RoomTemplate({ id, name, capacity, hotelId, occupied, se
     }
     setArrperson(aux);
   }, [selectedroom]);
-
-  return (
+  return(
     <Room invalid={capacity===occupied? 'true' : 'false'} select={selectedroom===id? 'true':'false'} onClick={() => { if(capacity!==occupied) { setSelectedroom(id);} }}>
       <p invalid={capacity===occupied? 'true':'false'}>{name}</p>
       <Vacancies>
